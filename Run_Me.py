9b85b97ad99c610c7ba974f479c5edf68fdd91d7
Siddhesh Agarwal <68057995+Siddhesh-Agarwal@users.noreply.github.com>
2020-12-15 12:02:19 +0530

    Update Run_Me.py

--- Run_Me.py
+++ Run_Me.py
@@ -1,5 +1,5 @@
 # Game_Arcade/Run_Me.py
-import tkinter as tk
+from tkinter import *
 from Games.Snake_Game import Play_Snake_Game
 from Games.Ping_Pong import Play_Ping_Pong
 from Games.Tic_Tac_Toe import Play_TicTacToe
@@ -9,6 +9,42 @@ def helper(function):
     root.destroy()
     function()
     
+def info():
+    def local_helper():
+        about.destroy()
+        menu()
+        
+    text = '''
+    GAME ARCADE
+    
+    This Project contains 4 games:
+    1) Ping Pong (2 player)
+    2) Snake game (single player)
+    3) Space battle (single player)
+    4) Tic Tac Toe (2 player)
+
+    Further details can be found by choosing a game and clicking on "about"
+    Button
+
+    Developed by     : Siddhesh Agarwal
+    '''
+
+    root.destroy()
+
+    about = Tk()
+    about.title("About")
+    about.resizable(0, 0)
+    
+    screen = Text(about, height=22, width=80)
+    screen.pack()
+    screen.config(borderwidth=20)
+    screen.insert(END, text)
+
+    BackButton = Button(about, btn_params, text="← Back", command=local_helper)
+    BackButton.place(x=165, y=245)
+        
+    about.mainloop()
+
 btn_params = {
     'padx': 16,
     'pady': 1,
@@ -21,22 +57,23 @@ btn_params = {
     'activebackground': 'yellow'
 }
 
-# Main Screen
-root = tk.Tk()
-root.title('GAME ARCADE')
-root.resizable(0, 0)
+def menu():
+    # Main Screen
+    global root
+    root = Tk()
+    root.title('GAME ARCADE')
+    root.config(borderwidth=5)
+    root.geometry('350x480+100+100')
+    root.resizable(0, 0)
 
-# Button for Snake Game
-game_btn_1 = tk.Button(root, btn_params, text='Snake Game', command=lambda: helper(Play_Snake_Game)).grid(row=0)
+    # Buttons
+    game_btn_1 = Button(root, btn_params, text='Snake Game', command=lambda: helper(Play_Snake_Game)).pack()
+    game_btn_2 = Button(root, btn_params, text='Ping Pong', command=lambda: helper(Play_Ping_Pong)).pack()
+    game_btn_3 = Button(root, btn_params, text='Tic-Tac-Toe', command=lambda: helper(Play_TicTacToe)).pack()
+    game_btn_4 = Button(root, btn_params, text='Space Battle', command=lambda: helper(Play_Space_Battle)).pack()
+    info_btn = Button(root, btn_params, text='Information', command=info).pack()
 
-# Button for Ping Pong
-game_btn_2 = tk.Button(root, btn_params, text='Ping Pong', command=lambda: helper(Play_Ping_Pong)).grid(row=1)
+    root.mainloop()
 
-# Button for Tic Tac Toe
-game_btn_3 = tk.Button(root, btn_params, text='Tic-Tac-Toe', command=lambda: helper(Play_TicTacToe)).grid(row=2)
-
-# Button for Space Battle
-game_btn_4 = tk.Button(root, btn_params, text='Space Battle', command=lambda: helper(Play_Space_Battle)).grid(row=3)
-
-# keeping screen open
-root.mainloop()
+if __name__ == '__main__':
+    menu()
